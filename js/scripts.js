9ed97f450f68c1ff78b2a9ada3b697366537a4b7
MarcusKyung <Kyungmj@gmail.com>
2023-03-31 11:25:09 -0700

    WIP: add js function for identifying numbers in array

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,11 @@ function translate(inputArray){
   // const contains1 = "Beep!"
   // const contains2 = "Boop!"
   // const contains3 = "Won't you be my neighbor?"
-  inputArray.map(let index = 0; index < array.length; index++) {
-    const element = array[index];
-    if(element.includes("1"))
-    console.log("contains1");
-  };
+  console.log(inputArray);
+  for (i = 0; i < inputArray.length; i++)
+    if (inputArray[i].includes(1)) {
+      console.log("true")
+  }
 };
 
 //UI Logic
@@ -27,7 +27,7 @@ function userInputFunction(event) {
   const convertedValue = translate(inputArray);
   document.getElementById("userInput").innerText = "User Inputted Number: " + userInput;
   document.getElementById("userInputCount").innerText = "User Inputted Number Counted Out: " + inputArray;
-  document.getElementById("userInputConverted").innerText = "User Inputted Number Converted: " + convertedValue;
+  // document.getElementById("userInputConverted").innerText = "User Inputted Number Converted: " + convertedValue;
 }
 
 window.addEventListener("load", function() {
