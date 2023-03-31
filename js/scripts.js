84180b1444da3ab84bd00dca8dded1bc5b33be64
MarcusKyung <Kyungmj@gmail.com>
2023-03-31 10:16:03 -0700

    WIP: add jsvariables for beep, boop, my neighbor

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,21 @@ function inputToArray(userInput){
   return inputArray;
 };
 
+function translate(inputArray){
+  const contains1 = "Beep!"
+  const contains2 = "Boop!"
+  const contains3 = "Won't you be my neighbor?"
+};
+
 //UI Logic
 function userInputFunction(event) {
   event.preventDefault();
   const userInput = document.getElementById("numberInput").value;
-  const inputArray = inputToArray(userInput)
+  const inputArray = inputToArray(userInput);
+  const convertedValue = translate(inputArray);
   document.getElementById("userInput").innerText = "User Inputted Number: " + userInput;
   document.getElementById("userInputCount").innerText = "User Inputted Number Counted Out: " + inputArray;
-  // document.getElementById("userInputConverted").innerText = "User Inputted Number Converted: ";
+  document.getElementById("userInputConverted").innerText = "User Inputted Number Converted: " + convertedValue;
 }
 
 window.addEventListener("load", function() {
